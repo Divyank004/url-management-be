8d2dbd5212a9003256b3464572f6eae974e8270f
Divyank Dhadi <divyank004@gmail.com>
2025-07-15 11:11:38 +0200

    add sample seed data into db

--- main.go
+++ main.go
@@ -5,16 +5,38 @@ import (
 	"github.com/gin-gonic/gin"
 	"url-management-be/config"
 	"url-management-be/models"
+	"time"
 )
 
 func setupRouter() *gin.Engine {
 
 	// Initialize database
 	config.ConnectDB()
-	
+
 	// Migrate the schema to db 
 	config.DB.AutoMigrate(&models.User{})
 	config.DB.AutoMigrate(&models.UrlAnalysisResult{})
+	// Seed sample data into db 
+	config.DB.Create(&models.User{
+		ID:         1,
+		Name:       "Divyank Dhadi",
+		Email:      "divyank004@gmail.com",
+		Password:   "password", // todo Hash the password
+		ValidFrom:  time.Now(),
+		ValidUntil: time.Date(2100, 1, 1, 0, 0, 0, 0, time.UTC),
+	})
+
+	config.DB.Create(&models.UrlAnalysisResult{
+		ID:             1,
+		URL:            "https://google.com",
+		Title:          "Google Search",
+		HTMLVersion:    "HTML5",
+		InternalLinks:  25,
+		ExternalLinks:  12,
+		Status:         "Running",
+		LoginForm:      true,
+	})
+
 	
 	r := gin.Default()
 
--- models/urlData.go
+++ models/urlData.go
@@ -3,8 +3,8 @@ package models
 type UrlAnalysisResult struct {
     ID     			int  	`json:"id" gorm:"primaryKey"`
     Title  			string  `json:"title" gorm:"not null" binding:"required"`
-    Url 			string  `json:"url" gorm:"not null" binding:"required"`
-    HtmlVersion 	string 	`json:"htmlVersion"`
+    URL 			string  `json:"url" gorm:"not null" binding:"required"`
+    HTMLVersion 	string 	`json:"htmlVersion"`
     InternalLinks 	int 	`json:"internalLinks"`
     ExternalLinks 	int 	`json:"externalLinks"`
 	Status 			string 	`json:"status"`
